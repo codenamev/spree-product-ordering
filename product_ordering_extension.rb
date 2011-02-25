9c1c98e77ccf8cddd49265ebbb986802ce4d9bda
Mosaic Interactive <valentino@mosaicwebsite.com>
2011-02-25 15:15:07 -0500

    Ordered products on taxon pages by 'position'

--- product_ordering_extension.rb
+++ product_ordering_extension.rb
@@ -13,6 +13,12 @@ class ProductOrderingExtension < Spree::Extension
   # end
   
   def activate
+    
+    Product.class_eval do
+      #acts_as_list
+      #default_scope order('position')
+      #named_scope :ordered, :order => 'position'
+    end
 
     # make your helper avaliable in all views
     # Spree::BaseController.class_eval do
